e47c96d4ae3bfcdf295f8ff1d860a48df7ec1337
Karimatif33 <101069884+Karimatif33@users.noreply.github.com>
2022-05-09 01:02:59 +0200

    JS learning

---
+++ main.js
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  elm.classList.add("prince")
+//  elm.innerText = ("welcome")
+//  ab.setAttribute("class", "prince");
+
+
+
+
+const ab = document.querySelector(".bleve")
+const elm = document.createElement("h1")
+const sec = document.querySelector(".sec")
+const buy = document.getElementById("buy")
+sec.append(elm);
+console.log("buy");
+
+// ab.remove()
+
+// setTimeout(() => {
+// elm.classList.add("prince")
+// elm.innerText =("wlcome funciton")
+// ab.setAttribute("class", "prince")
+// buy.style.backgroundColor=("red");
+
+// }
+// , 1000 )
+
+ const vaa = () => {
+
+    elm.classList.add("prince")
+    elm.innerText = ("welcome")
+    ab.setAttribute("class", "prince");
+   
+   
+}
+
+vaa ()
