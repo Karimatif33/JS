a534ba02d22d83c5ce1ec6e94599b749a89eac6f
Karimatif33 <101069884+Karimatif33@users.noreply.github.com>
2022-05-09 18:14:10 +0200

    aa

--- main.js
+++ main.js
@@ -6,6 +6,41 @@ const jsaa = document.querySelector(".jsaa")
 const btn = document.querySelector(".btn1")
 const con = document.querySelector(".bigger-contaier")
 const btnr = document.querySelector(".btn-primary1")
+const kh = document.querySelector(".kh")
+const container = document.querySelector(".container")
+const body = document.querySelector(".body")
+const mode = document.querySelector(".mode")
+const mode1 = document.querySelector(".mode1")
+
+mode.addEventListener("click" , (eo) => {
+    body.classList.add("dark")
+    mode.style.display="none"
+    mode1.style.display="block"
+
+})
+mode1.addEventListener("click" , (eo) => {
+    body.classList.remove("dark")
+    mode1.style.display="none"
+    mode.style.display="block"
+
+})
+card.addEventListener("click" , (eo) => {
+    con.classList.add("active")
+    kh.style.display="none"
+    eo.preventDefault()
+    
+})
+
+btnr.addEventListener("click" , (eo) => {
+    container.style.display="none"
+    kh.style.display="block"
+
+
+
+    setTimeout(() => {
+        con.classList.remove("active")
+    }, 2000 );
+})
 
 // card.addEventListener( "click" , (eo) => {
 //     // con.style.display = ("block") 
@@ -38,11 +73,3 @@ const btnr = document.querySelector(".btn-primary1")
 //     con.renoveAttribute ("close" ,"a")
 // })
 
-card.addEventListener("click" , (eo) => {
-    con.showModal()
-    eo.preventDefault()
-})
-
-btnr.addEventListener("click" , (eo) => {
-    con.close()
-})
