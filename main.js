c657035d8fd2e3cfd718b21a487560beb3f49217
Karimatif33 <101069884+Karimatif33@users.noreply.github.com>
2022-05-10 03:59:53 +0200

    aa

--- main.js
+++ main.js
@@ -1,75 +1,106 @@
-const card = document.querySelector(".card")
-console.log("card")
-const jsa = document.querySelector(".jsa")
-console.log("jsa")
-const jsaa = document.querySelector(".jsaa")
-const btn = document.querySelector(".btn1")
-const con = document.querySelector(".bigger-contaier")
-const btnr = document.querySelector(".btn-primary1")
-const kh = document.querySelector(".kh")
-const container = document.querySelector(".container")
-const body = document.querySelector(".body")
-const mode = document.querySelector(".mode")
-const mode1 = document.querySelector(".mode1")
 
-mode.addEventListener("click" , (eo) => {
-    body.classList.add("dark")
-    mode.style.display="none"
-    mode1.style.display="block"
 
-})
-mode1.addEventListener("click" , (eo) => {
-    body.classList.remove("dark")
-    mode1.style.display="none"
-    mode.style.display="block"
-
-})
-card.addEventListener("click" , (eo) => {
-    con.classList.add("active")
-    kh.style.display="none"
-    eo.preventDefault()
-    
-})
-
-btnr.addEventListener("click" , (eo) => {
-    container.style.display="none"
-    kh.style.display="block"
+const title = document.getElementsByTagName("h1")
+const tittleArray = Array.from(title)
+console.table(tittleArray)
 
 
 
-    setTimeout(() => {
-        con.classList.remove("active")
-    }, 2000 );
-})
 
-// card.addEventListener( "click" , (eo) => {
-//     // con.style.display = ("block") 
-//     con.classList.add("active")
-//     eo.preventDefault()
 
-// })
+tittleArray.forEach((Item) => {
+    tittleArray[0].style.color ="royalblue"
+    tittleArray[2].style.color ="red"
+    Item.style.backgroundColor="pink"
+});
 
-// btnr.addEventListener( "click" , (eo) => {
-//     // con.style.display = ("none")
-//     con.classList.remove("active")
-// })
 
-// card.addEventListener( "click" ,  (eo) => {
-//     jsa.style.display=("block")
-//     // jsa.setAttribute("class" , "jsaa")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const card = document.querySelector(".card")
+// console.log("card")
+// const jsa = document.querySelector(".jsa")
+// console.log("jsa")
+// const jsaa = document.querySelector(".jsaa")
+// const btn = document.querySelector(".btn1")
+// const con = document.querySelector(".bigger-contaier")
+// const btnr = document.querySelector(".btn-primary1")
+// const kh = document.querySelector(".kh")
+// const container = document.querySelector(".container")
+// const body = document.querySelector(".body")
+// const mode = document.querySelector(".mode")
+// const mode1 = document.querySelector(".mode1")
+
+// mode.addEventListener("click" , (eo) => {
+//     body.classList.toggle("dark")
+//     // mode.style.display="none"
+//     // mode1.style.display="block"
 
 // })
+// // mode1.addEventListener("click" , (eo) => {
+// //     body.classList.remove("dark")
+// //     mode1.style.display="none"
+// //     mode.style.display="block"
 
-
-// btn.addEventListener ( "click" , (eo) => {
-//     jsa.style.display=("none")
-
-// })
-
+// // })
 // card.addEventListener("click" , (eo) => {
-//     con.setAttribute ("open" ,"open")
+//     con.classList.add("active")
+//     kh.style.display="none"
+//     eo.preventDefault()
+    
 // })
+
 // btnr.addEventListener("click" , (eo) => {
-//     con.renoveAttribute ("close" ,"a")
+//     container.style.display="none"
+//     kh.style.display="block"
+
+
+
+//     setTimeout(() => {
+//         con.classList.remove("active")
+//     }, 2000 );
 // })
+
+// // card.addEventListener( "click" , (eo) => {
+// //     // con.style.display = ("block") 
+// //     con.classList.add("active")
+// //     eo.preventDefault()
+
+// // })
+
+// // btnr.addEventListener( "click" , (eo) => {
+// //     // con.style.display = ("none")
+// //     con.classList.remove("active")
+// // })
+
+// // card.addEventListener( "click" ,  (eo) => {
+// //     jsa.style.display=("block")
+// //     // jsa.setAttribute("class" , "jsaa")
+
+// // })
+
+
+// // btn.addEventListener ( "click" , (eo) => {
+// //     jsa.style.display=("none")
+
+// // })
+
+// // card.addEventListener("click" , (eo) => {
+// //     con.setAttribute ("open" ,"open")
+// // })
+// // btnr.addEventListener("click" , (eo) => {
+// //     con.renoveAttribute ("close" ,"a")
+// // })
 
