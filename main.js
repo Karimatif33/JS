650d30b0e0b95a3e0d87ca0f098e24fe3329b826
Karimatif33 <101069884+Karimatif33@users.noreply.github.com>
2022-05-12 00:01:52 +0200

    n

--- main.js
+++ main.js
@@ -8,13 +8,14 @@ uploud.addEventListener("click", (eo) => {
   eo.preventDefault();
   const upt = ` <div class="task task1" >
     <span><i class="fa-solid fa-star ops"></i></span>
-    <p>${input.value} </p>
+    <p class="text-text" >${input.value} </p>
   <!-- <span><i class="fa-solid fa-heart"></i></span>  -->
 
   <div class="icon">
-      <span><i class="fa-solid fa-trash-can"></i></span> 
+        <span><i class="fa-solid fa-trash-can"></i></span> 
+
       <span><i class="fa-solid fa-face-angry"></i></span> 
-  </div>  
+   </div> 
     
   </div>`;
 
