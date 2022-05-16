28d26adcdc7fa7ccd6f14c8601f229c73f30fded
Karimatif33 <101069884+Karimatif33@users.noreply.github.com>
2022-05-16 15:50:03 +0200

    .

--- main.js
+++ main.js
@@ -1,109 +1,67 @@
-const uploud = document.querySelector("button");
-const input = document.querySelector("input");
-const container = document.getElementById("container");
 
-uploud.addEventListener("click", (eo) => {
-    eo.preventDefault();
-    const upt = ` <div class="task task1" >
-    <span><i class="fa-solid fa-star ops"></i></span>
-    <p class="text-text" >${input.value} </p>
-  <!-- <span><i class="fa-solid fa-heart"></i></span>  -->
-  <div class="icon">
-        <span><i class="fa-solid fa-trash-can"></i></span> 
-      <span><i class="fa-solid fa-face-angry"></i></span> 
-   </div> 
-    
-  </div>`;
-    container.innerHTML += upt;
-    input.value = "";
-});
+// const updateTotalPrice = () => {
+//   const AllProducts = document.querySelectorAll(".product")
 
-container.addEventListener("click", (eo) => {
-    switch (eo.target.className) {
-        case "fa-solid fa-trash-can":
-            eo.target.parentElement.parentElement.parentElement.remove();
-            break;
+// let total = 0
 
-        case "fa-solid fa-face-angry":
-            eo.target.classList.add("unactive");
-            const heart = `<i class="fa-solid fa-heart"></i> `;
+// AllProducts.forEach(item => {
+//   const price  = Number(item.getElementsByClassName("price")[0].innerText.replace("$", ""))
+  
+  
+//   const quantity  = Number(item.getElementsByClassName("input-quantity")[0].value)
+  
 
-            eo.target.parentElement.parentElement.parentElement
-                .getElementsByClassName("text-text")[0]
-                .classList.add("text-e");
-
-            eo.target.parentElement.innerHTML += heart;
-            break;
-
-        case "fa-solid fa-heart":
-            eo.target.classList.remove("unactive");
-            eo.target.parentElement.parentElement.parentElement
-                .getElementsByClassName("text-text")[0]
-                .classList.remove("text-e");
-            eo.target.style.display = "none";
-            eo.target.parentElement
-                .getElementsByClassName("fa-face-angry")[0]
-                .classList.remove("unactive");
-            break;
-
-        case "fa-solid fa-star ops":
-            eo.target.classList.add("star");
-            container.prepend(eo.target.parentElement.parentElement);
-            break;
-
-        case "fa-solid fa-trash-can":
-            eo.target.parentElement.parentElement.parentElement.remove();
-            break;
-
-        case "fa-solid fa-star ops star":
-            eo.target.classList.remove("star");
-            container.append(eo.target.parentElement.parentElement);
-            break;
-
-        default:
-            break;
-    }
-});
-
-// container.addEventListener("click", (eo) => {
-
-//     if (eo.target.className == "fa-solid fa-trash-can") {
-//         eo.target.parentElement.parentElement.parentElement.remove();
-//     }
-
-//     else if (eo.target.className == "fa-solid fa-face-angry") {
-//         eo.target.classList.add("unactive");
-//         const heart = `<i class="fa-solid fa-heart"></i> `;
-
-//         eo.target.parentElement.parentElement.parentElement
-//             .getElementsByClassName("text-text")[0]
-//             .classList.add("text-e");
-
-//         eo.target.parentElement.innerHTML += heart;
-//     }
-
-//     else if (eo.target.className == "fa-solid fa-heart") {
-//         eo.target.classList.remove("unactive");
-//         eo.target.parentElement.parentElement.parentElement
-//             .getElementsByClassName("text-text")[0]
-//             .classList.remove("text-e");
-//         eo.target.style.display = ("none")
-//         eo.target.parentElement
-//             .getElementsByClassName("fa-face-angry")[0]
-//             .classList.remove("unactive");
-
-//     }
-
-//     else if (eo.target.className == "fa-solid fa-star ops") {
-//         eo.target.classList.add("star");
-//         container.prepend(eo.target.parentElement.parentElement)
-
-//     }
-
-//     else if (eo.target.className == "fa-solid fa-star ops star") {
-//         eo.target.classList.remove("star");
-//         container.append(eo.target.parentElement.parentElement)
-
-//     }
+//   total = total + (price*quantity)
 
 // });
+
+// document.getElementById("total-price").innerText = `  ${total}  $`
+// }
+
+
+
+
+
+
+//  const container = document.getElementById("container")
+//  container.addEventListener("change", (eo) => {
+   
+//   updateTotalPrice()
+//  })
+
+
+//  const calc = document.getElementById("calc")
+//  calc.addEventListener("click", (eo) => {
+   
+//   updateTotalPrice()
+//  })
+
+ 
+
+const container = document.getElementById("container")
+const calc = document.getElementById("calc")
+const allproduct = document.querySelectorAll(".product")
+
+
+
+calc.addEventListener("click" , (eo) => {
+const totalPrice = document.getElementById("total-price")
+    totalPrice.innerHTML=`  `  
+    let  total = 0
+    allproduct.forEach((item) => {
+        const price = Number (item.getElementsByClassName("price")[0].innerText.replace("$",""))  
+        const quantity = Number (item.getElementsByClassName("input-quantity")[0].value)
+            total = total + (price*quantity)
+            console.log(total)
+
+        });
+    
+})
+
+
+
+
+
+
+
+
